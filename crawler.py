1347bde2c67fa252043cae1c9246d08e7c87597d
Benjamin Hodges <benjaminhodges07@gmail.com>
2019-11-09 18:00:37 +0000

    multithreading

--- crawler.py
+++ crawler.py
@@ -4,6 +4,9 @@ import newspaper
 from newspaper import Article
 import os
 import time
+import threading
+from queue import Queue
+q=Queue()
 
 filepath = "../news_outlets.txt"
 downloadpath = "./data"
@@ -22,34 +25,57 @@ print("reading tweets files")
 #    linecount = i+1
 linecount = 39695156
 print(linecount, "tweets to processes")
-skipto = 233170
+skipto = 236430
 
 batchsize = 100
 
-def addtweet(tweet,idx):
+def flushQueue():
+    while q.qsize():
+        filename,fields = q.get()
+        with open(filename, 'a') as csvf:
+            writer = csv.writer(csvf,quoting=csv.QUOTE_ALL)
+            writer.writerow(fields)
+            print("Article added successfully:", fields[3])
+
+def addtweet(tweet):
     #tweet = api.get_status(tweetid)
     filename = downloadpath + "/" + tweet.author.id_str
     if len(tweet.entities["urls"]) == 1:
         link = tweet.entities["urls"][0]
         expanded = link["expanded_url"]
         if expanded.startswith("https://twitter.com/i/"):
-            print("Links is twitter status! Skipping")
+            #print("Links is twitter status! Skipping")
             return
         try:
             article = Article(expanded)
             article.download()
             article.parse()
             fields = [tweetid,tweet.created_at,expanded,article.title,article.authors,article.text]
-            with open(filename, 'a') as csvf:
-                writer = csv.writer(csvf,quoting=csv.QUOTE_ALL)
-                writer.writerow(fields)
-                print("Article added successfully:", article.title)
+            q.put((filename,fields))
         except (newspaper.article.ArticleException,ValueError) as e:
-            print(e)
+            pass
+            #print(e)
     else:
-        print("No links found!");
-    with open("status","w") as f:
-        f.write(str(idx))
+        pass
+        #print("No links found!");
+
+def gettweets(tweets):
+    try:
+        tweets = api.statuses_lookup(bulkids)
+    except tw.TweepError as e:
+        time.sleep(0.1)#avoid rate limiting
+        return
+
+    print("Retrieved ",len(tweets), "tweets")
+    threads = []
+    for tweet in tweets:
+        t = threading.Thread(target=addtweet, args = (tweet,) )
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()                
+        
+    flushQueue()
 
 with open(filepath,"r") as f:
     bulkids = []
@@ -60,18 +86,10 @@ with open(filepath,"r") as f:
         bulkids.append(tweetid)
 
         if len(bulkids) == batchsize:
-            try:
-                tweets = api.statuses_lookup(bulkids)
-                bulkids = []
-            except tw.TweepError as e:
-                print(e)
-                time.sleep(0.1)#avoid rate limiting
-            
-            for i,tweet in enumerate(tweets):
-                count = (idx+i+1) 
-                percent = count / linecount
-                print(tweetid,f"({count+1}/{linecount}({percent:.2f}))")
-                addtweet(tweet,count)
+            gettweets(bulkids)
+            bulkids = []
+            print(f"Progress {idx}/{linecount}" ,f"({idx/linecount:.2f})")
 
-
-
+        if os.path.exists("stop"):
+            print(idx)
+            exit()
